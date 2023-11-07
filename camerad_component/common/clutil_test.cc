d943b81119285f78b5a062a1f2230a28a1d97c21
tang <122545505+1121085292@users.noreply.github.com>
2023-11-07 10:32:53 +0800

    Delete camerad_component directory

--- camerad_component/common/clutil_test.cc
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-#include <CL/cl.h>
-
-#include "clutil.h"
-
-TEST(clutilTest, get_device_id_test){
-    cl_device_id device_id = cl_get_device_id(CL_DEVICE_TYPE_DEFAULT);
-    EXPECT_EQ(device_id, CL_SUCCESS);
-}
