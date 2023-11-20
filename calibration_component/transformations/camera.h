605ce30a0823edeb1464763d8016d71bb34edb4f
tang <122545505+1121085292@users.noreply.github.com>
2023-11-20 11:22:19 +0800

    Delete calibration_component directory

--- calibration_component/transformations/camera.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <eigen3/Eigen/Dense>
-
-#include "orientation.hpp"
-
-#define model_height 1.22
-
-Eigen::MatrixXd getViewFrameFromRoadFrame(double roll, double pitch, double yaw, double height);
