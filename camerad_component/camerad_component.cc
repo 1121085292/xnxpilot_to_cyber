72a4e32ebd09cc06d7d73be90329b8a466160a5e
tang <122545505+1121085292@users.noreply.github.com>
2023-12-06 11:56:45 +0800

    Update camerad_component.cc

--- camerad_component/camerad_component.cc
+++ camerad_component/camerad_component.cc
@@ -63,212 +63,45 @@ bool CameradComponent::Init() {
   // frame_rate_ = camera_conf_->frame_rate();           //  30
   // fps_ = camera_conf_->fps();                         //  20
 
-  // camera_writer_ = node_->CreateWriter<FrameData>(camera_conf_->camera_channel_name());
-  // thumbnail_writer_ = node_->CreateWriter<Thumbnail>(camera_conf_->thumbnail_channel_name());
+  camera_writer_ = node_->CreateWriter<FrameData>(camera_conf_->camera_channel_name());
+  thumbnail_writer_ = node_->CreateWriter<Thumbnail>(camera_conf_->thumbnail_channel_name());
  
   // camera init
   device_id = cl_get_device_id(CL_DEVICE_TYPE_GPU);
   // device_id = cl_get_device_id(CL_DEVICE_TYPE_DEFAULT);
   context = CL_CHECK_ERR(clCreateContext(NULL, 1, &device_id, NULL, NULL, &err));
-  vipc_server = VisionIpcServer("camerad", device_id, context);
-  // vipc_server.setVisionIpcServer("camerad", device_id, context);
-  // road_cam->buf.init(device_id, context, road_cam, &vipc_server, buffer_size_, 
-  //                   VisionStreamType::VISION_STREAM_RGB_BACK, VisionStreamType::VISION_STREAM_YUV_BACK);
+  // vipc_server = VisionIpcServer("camerad", device_id, context);
+  vipc_server.setVisionIpcServer("camerad", device_id, context);
 
-  // poll camera image data
-  // lister_ = apollo::cyber::Async(&VisionIpcServer::listener, &vipc_server);
-  
   async_result_ = apollo::cyber::Async(&CameradComponent::run, this);
-  // res_ = apollo::cyber::Async(&CameradComponent::processing_thread, this);
   return true;
 }
 
 void CameradComponent::run() {
     running_.exchange(true);
-
     cameras_init(&vipc_server, &cameras, device_id, context);
     cameras_open(&cameras);
     vipc_server.start_listener();
-    cameras_run(&cameras);
+    cameras_run(&cameras, thumbnail_writer_, camera_writer_);
 }
 
 CameradComponent::~CameradComponent() {
   if (running_.load()) {
     running_.exchange(false);
-    // lister_.wait();
     async_result_.wait();
-    // res_.wait();
   }
 }
 
-void CameradComponent::cameras_open(MultiCameraState *s) {
-  camera_open(&s->road_cam, true);
-  // camera_open(&s->driver_cam, true);
-}
-
-void CameradComponent::camera_open(CameraState *s, bool rear) {
-  // empty
-}
-
-// void CameradComponent::camera_close(CameraState *s) {
-//   // empty
+// std::string CameradComponent::gstreamer_pipeline(std::string sensor_mode, std::string sensor_id, std::string flip_method, 
+//                                 int display_width, int display_height, Format format, int framerate) {
+//     // sensor mode 1 = 1920 x 1080
+//     // "v4l2src device=/dev/video0 ! video/x-raw, width=(int)1920, height=(int)1536, format=(string)YUY2, framerate=(fraction)30/1 ! appsink"
+//     return sensor_mode + " device=" + sensor_id +
+//            " ! video/" + flip_method +
+//            ", width=(int)" + std::to_string(display_width) +
+//            ", height=(int)" + std::to_string(display_height) +
+//            ", format=(string)" + formatToString[format] +
+//            ", framerate=(fraction)" + std::to_string(framerate) +
+//            "/1 ! appsink";
 // }
 
-// void cameras_close(MultiCameraState *s) {
-//   camera_close(&s->road_cam);
-//   camera_close(&s->driver_cam);
-//   // delete s->pm;
-// }
-
-
-//此处需修改为实际使用的相机参数，包括相机型号和fps
-void CameradComponent::cameras_init(VisionIpcServer *v, MultiCameraState *s, cl_device_id device_id, cl_context ctx) {
-  camera_init(v, &s->road_cam, CAMERA_ID_ISX031, 30, device_id, ctx,
-              VISION_STREAM_RGB_BACK, VISION_STREAM_YUV_BACK);
-  // camera_init(v, &s->driver_cam, CAMERA_ID_AR0233, 30, device_id, ctx,
-  //              VISION_STREAM_RGB_FRONT, VISION_STREAM_YUV_FRONT);
-
-}
-
-void CameradComponent::camera_init(VisionIpcServer * v, CameraState *s, int camera_id, unsigned int fps, cl_device_id device_id, 
-                cl_context ctx, VisionStreamType rgb_type, VisionStreamType yuv_type) {
-  initializeCameraInfo();
-  assert(camera_id < static_cast<int>(std::size(cameras_supported)));
-  s->ci = cameras_supported[camera_id];
-  assert(s->ci.frame_width != 0);
-
-  s->camera_num = camera_id;
-  s->fps = fps;
-  
-  s->buf.init(device_id, ctx, s, v, FRAME_BUF_COUNT, rgb_type, yuv_type);
-  
-}
-
-void process_road_camera(MultiCameraState *s, CameraState *c, int cnt) {
-  const CameraBuf *b = &c->buf;
-  auto out_msg = std::make_shared<common_msgs::camerad::FrameData>();
-  fill_frame_data(b->cur_frame_data, out_msg);
-  out_msg->set_image(b->cur_yuv_buf->addr, b->cur_yuv_buf->len);
-  for(int i = 0; i < 9; i++){
-    out_msg->add_transform(b->yuv_transform.v[i]);
-  }
-  camera_writer_->Write(out_msg);
-}
-
-std::thread start_process_thread(MultiCameraState *cameras, CameraState *cs, process_thread_cb callback) {
-  return std::thread(processing_thread, cameras, cs, callback);
-//   return std::thread([this, cameras, cs, callback]() {
-//     this->processing_thread(cameras, cs, callback);
-// });
-
-}
-
-void CameradComponent::cameras_run(MultiCameraState *s) {
-  std::vector<std::thread> threads;
-  threads.push_back(start_process_thread(s, &s->road_cam, process_road_camera));
-
-  // threads.push_back(start_process_thread(s, &s->driver_cam, process_driver_camera));
-
-  std::thread t_rear = std::thread(road_camera_thread, &s->road_cam);
-  set_thread_name("mipi_thread");
-
-  // std::thread t_front = std::thread(driver_camera_thread, &s->driver_cam);
-  // set_thread_name("mipidriver_thread");
-  
-  for (auto &t : threads) t.join();
-  // t_front.join();
-  t_rear.join();
-
-  cameras_close(s);
-}
-
-
-
-void road_camera_thread(CameraState *s) {
-  set_thread_name("mipi_road_camera_thread");
-
-  // std::string pipeline = gstreamer_pipeline(
-  //   1, // sensor mode
-  //   0, // sensor id
-  //   2, // flip method
-  //   864, // width
-  //   486); // height
-
-  // camera-ar0233:
-  // cv::VideoCapture cap_road("v4l2src device=/dev/video0 ! video/x-raw, width=(int)1920, height=(int)1080,format=(string)YUY2, framerate=(fraction)30/1 ! appsink");
-  // camera-isx031:
-  //cv::VideoCapture cap_road("nvv4l2camerasrc device=/dev/video0 ! 'video/x-raw(memory:NVMM),format=YUY2,width=1920,height=1536,framerate=30/1' ! nvvidconv ! video/x-raw,format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink drop=1");
-  cv::VideoCapture cap_road("nvv4l2camerasrc device=/dev/video0 ! video/x-raw(memory:NVMM),format=YUY2, width=1920,height=1536,framerate=30/1 ! nvvidconv ! video/x-raw,format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink drop=1", cv::CAP_GSTREAMER);
-
-  // camera-ar0233:
-  // float ts[9] = {0.46414363, 0.0, 124.44721004,
-  //                 0.0, 0.46099291, 215.07801418,
-  //                 0.0, 0.0, 1.0};
-  // camera-isx031:
-  float ts[9] = {0.76528467, 0.0, -151.52535531,
-                  0.0, 0.76086957, -155.7173913,
-                  0.0, 0.0, 1.0};
-  run_camera(s, cap_road, ts);
-}
-
-void CameradComponent::run_camera(CameraState *s, cv::VideoCapture &video_cap, float *ts) {
-  assert(video_cap.isOpened());
-
-  cv::Size size(s->ci.frame_width, s->ci.frame_height);
-  const cv::Mat transform = cv::Mat(3, 3, CV_32F, ts);
-  uint32_t frame_id = 0;
-  size_t buf_idx = 0;
-  
-  while (!do_exit) {
-    cv::Mat YUV_mat, frame_mat, transformed_mat;
-    video_cap >> frame_mat;
-    // ar0233:
-    // COLOR_YUV2BGR_UYVY
-    // cv::cvtColor(YUV_mat, frame_mat, cv::COLOR_YUV2BGR_UYVY);
-    // isx031:
-    // COLOR_YUV2BGR_YUY2
-    //cv::cvtColor(YUV_mat, frame_mat, cv::COLOR_YUV2BGR_YUY2);
-
-    
-    cv::warpPerspective(frame_mat, transformed_mat, transform, size, cv::INTER_LINEAR, cv::BORDER_CONSTANT, 0);
-
-    s->buf.camera_bufs_metadata[buf_idx] = {.frame_id = frame_id};
-
-    auto &buf = s->buf.camera_bufs[buf_idx];
-    int transformed_size = transformed_mat.total() * transformed_mat.elemSize();
-
-    CL_CHECK(clEnqueueWriteBuffer(buf.copy_q, buf.buf_cl, CL_TRUE, 0, transformed_size, transformed_mat.data, 0, NULL, NULL));
-
-    s->buf.queue(buf_idx);
-    ++frame_id;
-    buf_idx = (buf_idx + 1) % FRAME_BUF_COUNT;
-  }
-}
-
-void *processing_thread(MultiCameraState *cameras, CameraState *cs, process_thread_cb callback) {
-  const char *thread_name = nullptr;
-  if (cs == &cameras->road_cam) {
-    thread_name = "RoadCamera";
-  } else if (cs == &cameras->driver_cam) {
-    thread_name = "DriverCamera";
-  } else {
-    thread_name = "WideRoadCamera";
-  }
-  set_thread_name(thread_name);
-
-  uint32_t cnt = 0;
-  while (!do_exit) {
-    if (!cs->buf.acquire()) continue;
-
-    callback(cameras, cs, cnt);
-
-    if (cs == &(cameras->road_cam) && thumbnail_writer_ && cnt % 100 == 3) {
-      // this takes 10ms???
-      publish_thumbnail(thumbnail_writer_, &(cs->buf));
-    }
-    cs->buf.release();
-    ++cnt;
-  }
-
-  return NULL;
-}
