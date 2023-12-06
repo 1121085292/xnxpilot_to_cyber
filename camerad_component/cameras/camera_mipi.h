d9067a85ba37939f4bf96c18a09d463514d39449
tang <122545505+1121085292@users.noreply.github.com>
2023-12-06 11:56:05 +0800

    Update camera_mipi.h

--- camerad_component/cameras/camera_mipi.h
+++ camerad_component/cameras/camera_mipi.h
@@ -6,29 +6,10 @@
 #include <CL/cl.h>
 #endif
 
-#include "camera_common.h"
+#include "perception/camerad_component/cameras/camera_common.h"
 
-CameraInfo cameras_supported[CAMERA_ID_MAX] = {
-  // road facing
-  // [CAMERA_ID_IMX477] = {
-  //     .frame_width = FRAME_WIDTH,
-  //     .frame_height = FRAME_HEIGHT,
-  //     .frame_stride = FRAME_WIDTH*3,
-  //     .bayer = false,
-  //     .bayer_flip = false,
-  // },
-  [CAMERA_ID_AR0233] = {
-      FRAME_WIDTH_FRONT,
-      FRAME_HEIGHT_FRONT,
-      FRAME_WIDTH_FRONT*3,
-      false,
-      false,
-  },
-  [CAMERA_ID_ISX031] = {
-      FRAME_WIDTH,
-      FRAME_HEIGHT,
-      FRAME_WIDTH*3,
-      false,
-      false,
-  },
-};
+void cameras_init(VisionIpcServer *v, MultiCameraState *s, cl_device_id device_id, cl_context ctx);
+void cameras_open(MultiCameraState *s);
+void cameras_run(MultiCameraState *s, std::shared_ptr<Writer<Thumbnail>>thumbnail_writer,
+                                          std::shared_ptr<Writer<FrameData>>camera_writer);
+void cameras_close(MultiCameraState *s);
